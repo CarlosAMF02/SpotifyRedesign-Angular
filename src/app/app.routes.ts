fac3c626f0e86abe4a18457122273a2d575f98a7
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2023-03-03 15:27:10 -0300

    [CHORE] [Authentication] Routes authentication control and player page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from "@angular/router";
+import { AuthenticationGuard } from "./guards/authentication.guard";
 
 export const AppRoutes: Routes =[
     {
         path: '',
-        redirectTo: 'login',
+        redirectTo: 'player',
         pathMatch: 'full'
     },
     {
         path: 'login',
         loadChildren: () => import('./pages/login/login.module').then(x => x.LoginModule)
+    },
+    {
+        path: 'player',
+        canLoad: [AuthenticationGuard],
+        loadChildren: () => import('./pages/player/player.module').then(x => x.PlayerModule)
     }
 ]
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { SpotifyConfiguration } from 'src/environments/environment';
 import Spotify from 'spotify-web-api-js'
+import { IUser } from '../interfaces/IUser';
+import { SpotifyUserToUser } from '../Common/spotifyHelper';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,32 @@ import Spotify from 'spotify-web-api-js'
 export class SpotifyService {
 
   spotifyApi : Spotify.SpotifyWebApiJs = null;
+  user: IUser;
 
   constructor() { 
     this.spotifyApi = new Spotify()
+  }
+
+  async initializeUser() {
+    if(!!this.user) return true;
+    
+    const token = localStorage.getItem('token');
+
+    if(!token) return false;
+
+    try {
+      this.setAccessToken(token);
+      await this.getSpotifyUser();
+      return !!this.user;
+    } catch(ex) {
+      console.error(ex);
+      return false;
+    }
+  }
+
+  async getSpotifyUser() {
+    const userInfo = await this.spotifyApi.getMe();
+    this.user = SpotifyUserToUser(userInfo)
   }
 
   getLoginUrl() {
